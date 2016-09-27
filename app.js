9c1b77e5588905d87a646f833c62bff8c0e6789b
Joseph <joseph.jr@gmail.com>
2016-09-27 10:57:23 +0100

    note.html is page

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var httpServer = require("http-server");
 var path = require("path");
 
-var pathToHtmlAndJsFiles = path.join(__dirname, ".");
+var pathToHtmlAndJsFiles = path.join(__dirname, "note.html");
 var server = httpServer.createServer({ root: pathToHtmlAndJsFiles });
 server.listen(3000);
