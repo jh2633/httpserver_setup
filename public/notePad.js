5940ca0e75b20cd29fdc11d31249b380e2a2c1b5
Thao Vo <ttthao.vo@gmail.com>
2016-09-28 12:34:41 +0100

    Implementing functionality to noePad model

--- public/notePad.js
+++ public/notePad.js
@@ -7,20 +7,33 @@ Notepad.prototype.note = function(){
   .getElementById("create")
     .addEventListener("click", function(clickEvent){
       clickEvent.preventDefault();
-      Notepad.prototype.abbrv();
+      Notepad.prototype.list();
+      Notepad.prototype.create();
     });
 };
 
-Notepad.prototype.abbrv = function(){
+Notepad.prototype.create = function(){
+  var para = document.createElement("p");
+  var p = document.getElementById("para");
+  para.appendChild(document.createTextNode(document.getElementById("content").value));
+  p.appendChild(para);
+};
+
+Notepad.prototype.abbrNote = function (note) {
+  if(note.length > 20) {
+    return note.substring(0,19)+"...";
+  } else {
+    return note;
+  }
+};
+
+Notepad.prototype.list = function(){
   var ul = document.getElementById("abbrv");
   var li = document.createElement("li");
   var a = document.createElement("a");
-  var para = document.createElement("p");
-  var p = document.getElementById("para");
+  var abNote = document.createTextNode(Notepad.prototype.abbrNote(document.getElementById("content").value));
   a.setAttribute('href', "http://google.com");
-  a.appendChild(document.createTextNode(document.getElementById("content").value));
+  a.appendChild(abNote);
   li.appendChild(a);
   ul.appendChild(li);
-  para.appendChild(document.createTextNode(document.getElementById("content").value));
-  p.appendChild(para);
 };
